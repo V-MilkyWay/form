6cae46d1f44bd16ddc406af21e4e233d73eb5945
Vladislav Milushkov <radshura@yandex.ru>
2021-04-27 19:44:41 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,23 @@ let formInput = formSender.querySelectorAll('.form-sender__input');
 
 let formButton = formSender.querySelector('.form-sender__button');
 
+let plusButton = formSender.querySelectorAll('.form-sender__plus-button');
+
+
+function plus() {
+    formSender.insertAdjacentHTML('beforeend', `<p class="form-sender__title" style="padding-top: 10px; border-top: 1px dotted green>Объект:</p>
+    <input class="form-sender__input" type="text" name="OBJECT" placeholder="Колодец" value="">
+    <p class="form-sender__title">Ширина:</p>
+    <input class="form-sender__input" type="text" name="WIDTH" placeholder="1000см" value="">
+    <p class="form-sender__title">Глубина:</p>
+    <input class="form-sender__input" type="text" name="DEEP" placeholder="4321см" value="">
+    <p class="form-sender__title">Наличие дна:</p>
+    <div class="form-sender__switch_type_btn form-sender__switch_type_on"></div>
+    <p class="form-sender__title">Примечание:</p>
+    <textarea type="textarea" style="width: 234px; height: 230px; margin-top: -20px;" placeholder="Введите текст..." value=" " name="primechanie"></textarea>`);
+};
+
+formSender.addEventListener('click', plus);
 
 function saveButton(evt) {
     evt.preventDefault();
