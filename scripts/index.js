c535b9d6bb488e55102bbcef7f3f64584ae81aad
Vladislav Milushkov <radshura@yandex.ru>
2021-04-27 23:57:41 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -8,8 +8,31 @@ let formButton = post.querySelector('.form-sender__button');
 
 let plusButton = post.querySelectorAll('.form-sender__plus-button');
 
-
 let i = 1;
+
+
+$(document).on("click", ".form-sender__switch_type_btn", function() {
+
+    $(this).toggleClass('form-sender__switch_type_on');
+    if ($(this).hasClass('form-sender__switch_type_on')) {
+        $(this).trigger('on.switch');
+    } else {
+        $(this).trigger('off.switch');
+    }
+});
+
+$(document)
+
+let out6 = "Да";
+
+$(document).on('on.switch', function() {
+    console.log('Кнопка переключена в состояние on');
+    out6 = "Да"
+});
+$(document).on('off.switch', function() {
+    console.log('Кнопка переключена в состояние off');
+    out6 = "Нет"
+});
 
 $(document).on("click", ".form-sender__plus-button", function() {
     i = i + 1;
@@ -33,37 +56,16 @@ $(document).on("click", ".form-sender__button", function() {
     out3 = formInput[2];
     out4 = formInput[3];
     out5 = formInput[4];
-    out6 = formInput[5];
 
 
     alert(`
-    Заказ: 1
+    Заказ №: ${i}
     Контактный телефон: ${out.value}
     Название фирмы: ${out2.value}
 
     ---Информация о заказе---
     Объект: ${out3.value}
     Ширина: ${out4.value}
-    Глубина:${out5.value}`);
-});
-
-
-
-$(document).on("click", ".form-sender__switch_type_btn", function() {
-
-    $(this).toggleClass('form-sender__switch_type_on');
-    if ($(this).hasClass('form-sender__switch_type_on')) {
-        $(this).trigger('on.switch');
-    } else {
-        $(this).trigger('off.switch');
-    }
-});
-
-$(document)
-
-$(document).on('on.switch', function() {
-    console.log('Кнопка переключена в состояние on');
-});
-$(document).on('off.switch', function() {
-    console.log('Кнопка переключена в состояние off');
+    Глубина:${out5.value}
+    Наличие дна:${out6}`);
 });
