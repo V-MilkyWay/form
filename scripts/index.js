0a068cea2206ab98b4e42cf3aa8e6432f7903f98
Vladislav Milushkov <radshura@yandex.ru>
2021-04-29 01:25:07 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,12 @@ let page = document.querySelector('.page');
 let main = document.querySelector('.main');
 let formSender = main.querySelector('.form-sender');
 let post = main.querySelector('.post');
-let formInput = formSender.querySelectorAll('.form-sender__input');
 
 let formButton = post.querySelector('.form-sender__button');
 
 let plusButton = post.querySelectorAll('.form-sender__plus-button');
 
 let i = 1;
-
 
 $(document).on("click", ".form-sender__switch_type_btn", function() {
 
@@ -33,10 +31,10 @@ $(document).on('off.switch', function() {
     console.log('Кнопка переключена в состояние off');
     out6 = "Нет"
 });
-
+let id;
+id = 1;
 $(document).on("click", ".form-sender__plus-button", function() {
     i = i + 1;
-
     formSender.insertAdjacentHTML('beforeend', `
     <h2 class="form-sender__title" style="text-align: center; margin-bottom: -40px; text-decoration: underline;">Колодец №:${i}</h2>
     <p class="form-sender__title" style="padding-top: 10px; border-top: 1px dotted green>Объект:</p>
@@ -49,23 +47,51 @@ $(document).on("click", ".form-sender__plus-button", function() {
     <div class="form-sender__switch_type_btn form-sender__switch_type_on"></div>`);
 });
 
-
+const mails = [];
 $(document).on("click", ".form-sender__button", function() {
-    let out = formInput[0];
-    out2 = formInput[1];
-    out3 = formInput[2];
-    out4 = formInput[3];
-    out5 = formInput[4];
+    let formInput = formSender.querySelectorAll('.form-sender__input');
+    for (elem of formInput) {
+        mails.push(elem.value);
+    }
 
 
+
+    const fio = mails.slice(0, 1).map(el => `ФИО: ${el}`);
+    const phone = mails.slice(1, 2).map(el => `Контактный телефон: ${el}`);
+    const company = mails.slice(2, 3).map(el => `Название фирмы:  ${el}`);
+
+    const width = mails.filter(function(v, i) { if ((i % 2 != 0) && (i > 2)) return v; }).map(el => `Ширина: ${el} `);
+    const deep = mails.filter(function(v, i) { if ((i % 2 === 0) && (i > 3)) return v; }).map(el => `Глубина: ${el}`);
+
+    console.log(fio + " " + phone + " " + company + " " + width + " " + deep);
     alert(`
     Заказ №: ${i}
-    ФИО: ${out.value}
-    Контактный телефон: ${out2.value}
-    Название фирмы: ${out3.value}
+    ${fio}
+    ${ phone}
+    ${company}
 
     ---Информация о заказе---
-    Ширина: ${out4.value}
-    Глубина: ${out5.value}
+    ${width}
+    ${deep}
     Наличие дна: ${out6}`);
+    /*
+                let out = mails[0];
+                out2 = mails[1];
+                out3 = mails[2];
+                out4 = mails[3];
+                out5 = mails[4];
+
+                let result = [`
+                    Заказ №: ${i}
+                    ФИО: ${out}
+                    Контактный телефон: ${out2}
+                    Название фирмы: ${out3}
+
+                    ---Информация о заказе---
+                    Ширина: ${out4}
+                    Глубина: ${out5}
+                    Наличие дна: ${out6}`]
+
+                alert(result);
+*/
 });
