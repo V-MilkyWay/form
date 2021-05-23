1b6ae0195a3f7196612bdf9935cb94f6eaa10fc7
Vladislav Milushkov <radshura@yandex.ru>
2021-05-23 10:40:00 +0300

    комментарий к коммиту

--- scripts/index.js
+++ scripts/index.js
@@ -30,18 +30,6 @@ function closePopup(popup) {
     popup.classList.remove('popup_opened');
 };
 
-
-
-$(document).on("click", ".form-sender__switch_type_btn", function() {
-
-    $(this).toggleClass('form-sender__switch_type_on');
-    if ($(this).hasClass('form-sender__switch_type_on')) {
-        $(this).trigger('on.switch');
-    } else {
-        $(this).trigger('off.switch');
-    }
-});
-
 //123gi
 $(document)
 
@@ -54,47 +42,47 @@ function plus(p) {
 }
 
 $(document).on("click", ".form-sender__button", function() {
-    let formInput = formSender.querySelectorAll('.form-sender__input');
-    let formTextarea = post.querySelector('.post__textarea');
-    let textArea = formTextarea.value;
-    let formInputBottom = formSender.querySelectorAll('.form-sender__input_bottom');
+            let formInput = formSender.querySelectorAll('.form-sender__input');
+            let formTextarea = post.querySelector('.post__textarea');
+            let textArea = formTextarea.value;
+            let formInputBottom = formSender.querySelectorAll('.form-sender__input_bottom');
 
-    for (elem of formInput) {
-        mails.push(elem.value);
-    }
+            for (elem of formInput) {
+                mails.push(elem.value);
+            }
 
-    for (elem of formInputBottom) {
-        mailsBottom.push(elem.value);
-    }
-
-
-    const fio = mails.slice(0, 1).map(el => `ФИО: ${el}`);
-    const phone = mails.slice(1, 2).map(el => `Контактный телефон: ${el}`);
-    const company = mails.slice(2, 3).map(el => `Название фирмы:  ${el}`);
-    const bottom = mailsBottom.map(el => `  Тип-№${(num2 = num2 + 1)}: ${el}  `);
+            for (elem of formInputBottom) {
+                mailsBottom.push(elem.value);
+            }
 
 
-    const width = mails.filter(function(v, i) { if ((i % 2 != 0) && (i > 2)) return v; }).map(el => `  Диаметр-№${(num = num + 1)}: ${el} `);
-    const deep = mails.filter(function(v, i) { if ((i % 2 === 0) && (i > 3)) return v; }).map(el => `  Глубина-№${(num1 = num1 + 1)}: ${el} `);
+            const fio = mails.slice(0, 1).map(el => `ФИО: ${el}`);
+            const phone = mails.slice(1, 2).map(el => `Контактный телефон: ${el}`);
+            const company = mails.slice(2, 3).map(el => `Название фирмы:  ${el}`);
+            const bottom = mailsBottom.map(el => `
+    Тип: ${el}  `);
+
+
+            const width = mails.filter(function(v, i) { if ((i % 2 != 0) && (i > 2)) return v; }).map(el => `
+    Заказ-№${(num2 = num2 + 1)}
+    Диаметр: ${el} `);
+            const deep = mails.filter(function(v, i) { if ((i % 2 === 0) && (i > 3)) return v; }).map(el => `
+    Глубина: ${el} `);
 
 
 
-    resetForm(formSender);
-    openPopup(popupEditProfile);
-    alert(`
+            let result = width.map((elem, index) => `${elem}, ${deep[index]}, ${bottom[index]} `);
+
+            resetForm(formSender);
+            openPopup(popupEditProfile);
+            alert(`
     Количество колодцев: ${i}
     ${fio}
     ${ phone}
     ${company}
-
     ---Информация о заказе---
-
-    ${width}
-
-    ${deep}
-
-    ${bottom}
-    
+    ${result.join(`
+    -------------------------`)}
     ---Примечание---
     ${textArea}`);
 
